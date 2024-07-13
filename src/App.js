dbabc99e6aaafd205608a3780922dada6309f19f
raghavgupta2412 <raghav.gupta_cs21@gla.ac.in>
2024-07-13 10:12:42 +0530

    learing useReducer hook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Loader from "./Loader";
+export default function App() {
+  return (
+    <div className="app">
+      {/* <Header />
+      <Loader />
+       */}
+      <DateCounter />
+    </div>
+  );
+}
